ff236f3b32bd26c33ee997cfcd956885caf15012
Sanket Manmode <manmodesanket@gmail.com>
2020-04-23 11:39:03 +0530

    working properly

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-
-const status = document.querySelector(".grid");
-
 let gameOn = true;
 
 let currentPlayer = "X";
 
 let gameState = ["", "", "", "", "", "", "", "", ""];
 
+let winner = "";
+
+function restartGame() {
+    //console.log("restart");
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    gameOn = true;
+    currentPlayer = "X";
+    document.querySelectorAll(".cell").forEach(cell => {
+        cell.innerHTML = "";
+    });
+    document.querySelector(".result").innerHTML = "";
+}
 
 function handlePlayerChange() {
     if(currentPlayer === "X") {
@@ -18,11 +27,38 @@ function handlePlayerChange() {
 }
 
 function handleResultValidation() {
+    //console.log("result evaluation");
+    const winningCondition = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7]
+    ];
+    for(let i = 0; i < winningCondition.length; i = i + 1) {
+        //console.log("in i");
+        let count = 0;
+        for(let j = 0; j < winningCondition[i].length; j = j + 1) {
+            //console.log("in j");
+            if(gameState[winningCondition[i][j]] == currentPlayer) {
+                count += 1;
+            }
+        }
+        if(count === 3) {
+            winner = currentPlayer;
+            gameOn = false;
+            document.querySelector(".result").innerHTML = currentPlayer + " has won the game";
+            break;
+        }
+    }
     handlePlayerChange();
 }
 
 function handleCellPlayed(clickedCell, clickedCellIndex) {
-    console.log("in handle cell");
+    //console.log("in handle cell");
     gameState[clickedCellIndex] = currentPlayer;
     clickedCell.innerHTML = currentPlayer;
 }
@@ -30,13 +66,13 @@ function handleCellPlayed(clickedCell, clickedCellIndex) {
 
 function handleCellClick(clickedCellEvent) {
     const clickedCell = clickedCellEvent.target;
-    console.log("clicked");
+    //console.log("clicked");
     const clickedCellIndex = parseInt(
         clickedCell.getAttribute("cell-id")
     );
-    console.log(clickedCellIndex);
+    //console.log(clickedCellIndex);
     if (gameState[clickedCellIndex] === "X" || gameState[clickedCellIndex] === "O" || !gameOn) {
-        console.log("not allowed")
+        //console.log("not allowed")
         return;
     }
     handleCellPlayed(clickedCell, clickedCellIndex);
@@ -47,3 +83,7 @@ function handleCellClick(clickedCellEvent) {
 document.querySelectorAll(".cell").forEach(cell => {
     cell.addEventListener('click', handleCellClick);
 });
+
+
+
+document.getElementById("restart").addEventListener('click',restartGame);
